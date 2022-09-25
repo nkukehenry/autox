0a519aaa0ed1a09351f9fc97298648b667b67b79
Henry Nkuke <henry.nkuke@interswitchng.com>
2022-09-25 12:26:57 +0300

    Added Edit capabilities

--- application/views/admin/edit_product.php
+++ application/views/admin/edit_product.php
@@ -98,7 +98,7 @@
 
                             </div>                             
                         
-                            <button type="submit" class="btn btn-info btn-fill pull-right">Add Product</button>
+                            <button type="submit" class="btn btn-info btn-fill pull-right">Save Changes</button>
                             <div class="clearfix"></div>
                         </form>
                     </div>
